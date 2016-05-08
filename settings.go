57af796a8b0960283b87033d0cdaa2a05828c837
John Morrice <john@functorama.com>
2016-05-08 21:56:47 +0100

    Final checkin of settings

--- settings.go
+++ settings.go
@@ -1,14 +1,13 @@
 package main
 
-// These settings are by default good for development
+// These settings are good for development
 const __ADDR = "godelbrot.functorama.mor"
 const __PORT = 9898
 const __PREFIX = ""
-const __TICKTIME = 100 
+const __TICKTIME = 10
 const __DEBUG = true
 
 // These settings are better for running an internet demo
-
 /*
 const __ADDR = "godelbrot.functorama.com"
 const __PORT = 80
