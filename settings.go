acd318116770b2f41f97fc4c11dc98622cdcbc1e
John Morrice <john@functorama.com>
2016-07-11 22:15:56 +0100

    Update settings

--- settings.go
+++ settings.go
@@ -1,22 +1,22 @@
 package main
 
 // These settings are good for development
+/*
 const __ADDR = "localhost"
 const __PORT = 9898
 const __PREFIX = ""
 const __TICKTIME = 100
 const __DEBUG = true
 const __TRACE = false
+*/
 
 // These settings are better for running an internet demo
-/*
 const __ADDR = "godelbrot.functorama.com"
 const __PORT = 80
 const __PREFIX = ""
 const __TICKTIME = 250
 const __DEBUG = false
 const __TRACE = false
-*/
 
 // ADVANCED SETTINGS
 const __RESIZE_MS = 300
